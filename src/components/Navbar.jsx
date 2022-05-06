f6f9a4f354624df999978cc4e48614ab9d251407
Amanda Ailin <amandaailindev@gmail.com>
2022-05-06 12:49:27 -0300

    Primeiro commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container =  styled.div`
+    height: 60px;
+    background-color: black;
+`
+
+
+const Navbar = () => {
+    return (
+        <Container>
+            navbar
+        </Container>
+    )
+}
+
+export default Navbar
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const Home = () => {
+    return (
+        <div className="container">
+            <Navbar/>
+        </div>
+    )
+}
+
+export default Home
